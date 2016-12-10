a0abca59de61f3e673b7ae5bdb1b76cb9a46263f
Ankur Singh <ankur13019@iiitd.ac.in>
2016-12-10 18:59:52 +0530

    secure websocket support and fixes

--- app.rb
+++ app.rb
@@ -11,5 +11,11 @@ module CloudTimer
     get "/admin" do
     	erb :admin
     end
+
+    get "/assets/js/application.js" do
+      content_type :js
+      @scheme = ENV['RACK_ENV'] == "production" ? "wss://" : "ws://"
+      erb :"application.js", layout: nil
+    end
   end
 end
